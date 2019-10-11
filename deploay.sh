a12aa8a0c7fa918ab896288ecdc1c30e953d863a
fans <fan.jinlong@qq.com>
2019-10-11 15:03:42 +0800

    update

--- deploay.sh
+++ deploay.sh
@@ -1,6 +1,6 @@
 # 自动部署
 kill -9 $(pgrep webserver)
-cd ~/Go-DevOps/
+cd /Users/fanjinlong/dev/Go/research/Go-DevOps
 git pull git@github.com:jinyuyoulong/Go-DevOps.git
 cd fan-webserver/
 go build #如果在本地交叉编译的话，不用执行这行代码。
