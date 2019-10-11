e66647c7bc7eb61b7a5aef8eeaea25274194f914
fans <fan.jinlong@qq.com>
2019-10-11 15:05:55 +0800

    update

--- fan-webserver/main.go
+++ fan-webserver/main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func firstPage(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "<h1> hello , My first deploy page  </h1>")
+	io.WriteString(w, "<h1> hello , My first deploy page 1 </h1>")
 }
 func main() {
 	http.HandleFunc("/", firstPage)
